67f3e0fe205828e2835b78f710370b2b754e5a05
melloradam97 <melloradam87@gmail.com>
2020-11-05 14:06:05 +0000

    changed start script

--- src/App.js
+++ src/App.js
@@ -85,7 +85,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('https://floating-anchorage-53282.herokuapp.com:3001/image', {
+          fetch('https://floating-anchorage-53282.herokuapp.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
